5c407b3dfe5e269d348e2cd2fb423a96c6f6137e
Iman <boubalishgames@gmail.com>
2016-11-05 19:05:33 -0400

    Fix#4

--- Assets/Scripts/Inventory Scripts/Weapons Inventory/Pistols.cs
+++ Assets/Scripts/Inventory Scripts/Weapons Inventory/Pistols.cs
@@ -12,33 +12,17 @@ public class Pistols : MonoBehaviour
 
     //Pistols Button
     private Button PistolsButton;
-    private string PistolsButtonPath = "_Inventory/_Background/_Weapons Panel/_Pistols";
+    private string PistolsButtonPath = "_Inventory/_Background/_Weapons SubBar/_Pistols";
 
-    //Pistols Back Button
-    public bool PistolBackToggle;
-    private Button PistolsBackButton;
-    private string PistolsBackButtonPath = "_Inventory/_Background/_Pistols Panel/_Back";
-
-    //Pistols Back To Inventory Button
-    private bool PistolsBackInventoryToggle;
-    private Button PistolsBackInventoryButton;
-    private string PistolsBackInventoryPath = "_Inventory/_Background/_Pistols Panel/_Back To Inventory";
 
 	void Awake ()
 	{
         //Pistols Panel
 	    PistolsPanel = GameObject.Find(PistolsPanelPath);
+
 	    //Pistols Button
-		//PistolsButton = GameObject.Find(PistolsButtonPath).GetComponent<Button>();
-		//PistolsButton.onClick.AddListener(() => PistolsControls());
-
-        //Pistols Back Button
-	    //PistolsBackButton = GameObject.Find(PistolsBackButtonPath).GetComponent<Button>();
-	   // PistolsBackButton.onClick.AddListener(() => PistolsBack());
-
-	    //Pistols Back To Inventory
-	  //  PistolsBackInventoryButton = GameObject.Find(PistolsBackInventoryPath).GetComponent<Button>();
-	  //  PistolsBackInventoryButton.onClick.AddListener(() => PistolsBTIN());
+		PistolsButton = GameObject.Find(PistolsButtonPath).GetComponent<Button>();
+		PistolsButton.onClick.AddListener(() => PistolsControls());
 	}
 
     void Update() { }
@@ -59,44 +43,7 @@ public class Pistols : MonoBehaviour
 
         else if(PistolsToggle == false)
         {
-            PistolsPanel.SetActive(false);
-        }
-	}
 
-	public void PistolsBack ()
-	{
-	    PistolBackToggle = !PistolBackToggle;
-
-        if(PistolBackToggle == true)
-        {
-            //Pistols
-            PistolsPanel.SetActive(false);
-            PistolsToggle = false;
-
-            //Weapons Inventory
-            WeaponsInventory.weaponsInventory.WeaponsInventoryToggle = true;
-            WeaponsInventory.weaponsInventory.WeaponsInventoryPanel.SetActive(true);
-
-            PistolBackToggle = false;
-        }
-	}
-
-	//Back To Inventory Button
-	public void PistolsBTIN ()
-	{
-	    PistolsBackInventoryToggle = !PistolsBackInventoryToggle;
-
-        if(PistolsBackInventoryToggle == true)
-        {
-            //Pistols
-            PistolsPanel.SetActive(false);
-            PistolsToggle = false;
-
-            //Inventory
-            Inventory.inventory.InventoryToggle = true;
-            Inventory.inventory.InventoryPanel.SetActive(true);
-
-            PistolsBackInventoryToggle = false;
         }
 	}
 }
